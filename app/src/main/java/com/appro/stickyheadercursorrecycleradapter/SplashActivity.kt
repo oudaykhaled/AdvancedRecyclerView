311bca8e9814059b064beb8513dc9cf1a212a7d1
oudaykhaled <ouday.khaled@gmail.com>
2019-11-16 01:11:30 +0300

    Remove unused files

--- app/src/main/java/com/appro/stickyheadercursorrecycleradapter/SplashActivity.kt
+++ app/src/main/java/com/appro/stickyheadercursorrecycleradapter/SplashActivity.kt
@@ -21,8 +21,6 @@ class SplashActivity : AppCompatActivity() {
 
     internal lateinit var chatModels: ArrayList<ChatModel>
 
-    //    @BindView(R.id.recyclerView)
-    //    RecyclerView<TestAdapter> chatsRecyclerview;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -78,10 +76,7 @@ class SplashActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        //        if (getIntent().hasExtra("showBlock")) {
         menuInflater.inflate(R.menu.menu_chat1, menu)
-
-        //        }
         return super.onCreateOptionsMenu(menu)
     }
 
@@ -94,62 +89,6 @@ class SplashActivity : AppCompatActivity() {
             }
         }
 
-    }
-
-    companion object {
-
-
-        val LOADER_TYPE_CHAT = 1
-
-
-        fun getTimeString(then: Long): String {
-            val date = Date(then)
-
-            val dateStr = StringBuffer()
-
-            val calendar = Calendar.getInstance()
-            calendar.time = date
-            val now = Calendar.getInstance()
-
-            val days = daysBetween(calendar.time, now.time)
-            val minutes = hoursBetween(calendar.time, now.time)
-            val hours = minutes / 60
-            return getDate(then, "h:mm aa")
-        }
-
-        fun daysBetween(d1: Date, d2: Date): Int {
-            return ((d2.time - d1.time) / DateUtils.DAY_IN_MILLIS).toInt()
-        }
-
-        fun minuteBetween(d1: Date, d2: Date): Int {
-            return ((d2.time - d1.time) / DateUtils.SECOND_IN_MILLIS).toInt()
-        }
-
-        fun hoursBetween(d1: Date, d2: Date): Int {
-            return ((d2.time - d1.time) / DateUtils.MINUTE_IN_MILLIS).toInt()
-        }
-
-        fun getDate(milliSeconds: Long, dateFormat: String): String {
-            // Create a DateFormatter object for displaying date in specified format.
-            val formatter = SimpleDateFormat(dateFormat)
-
-            // Create a calendar object that will convert the date and time value in milliseconds to date.
-            val calendar = Calendar.getInstance()
-            calendar.timeInMillis = milliSeconds
-            return formatter.format(calendar.time)
-        }
-
-
-        fun <T> initLoader(loaderId: Int, args: Bundle, callbacks: LoaderManager.LoaderCallbacks<T>,
-                           loaderManager: LoaderManager) {
-            val loader = loaderManager.getLoader<T>(loaderId)
-            if (loader != null && loader.isReset) {
-                loaderManager.restartLoader(loaderId, args, callbacks)
-            } else {
-                loaderManager.initLoader(loaderId, args, callbacks)
-            }
-
-        }
     }
 
 }
